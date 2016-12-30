17c7de1d3c952559739715978201aca4c2ed8b30
LuluffyLee <494211175@qq.com>
2016-12-30 16:53:43 +0800

    计数前增加滤波

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -169,6 +169,9 @@ void MainWindow::manualCount()
         cv::Mat resultImage = QImageToMat(*image);
         cv::Mat imageShold;
         threshold(resultImage, imageShold, ui->lcdThreshold->value(), 255, CV_THRESH_BINARY);   // 必须进行二值化
+
+        medianBlur(imageShold,imageShold,3);//中值滤波
+
         vector<vector<cv::Point>> contours;
         //CV_CHAIN_APPROX_NONE  获取每个轮廓每个像素点
         findContours(imageShold, contours, CV_RETR_CCOMP, CV_CHAIN_APPROX_NONE, cvPoint(0, 0));
@@ -647,3 +650,36 @@ void MainWindow::blur(){
                              QMessageBox::Yes, QMessageBox::Yes);
     }
 }
+
+
+//高斯滤波
+        //src:输入图像
+        //dst:输出图像
+        //Size(5,5)模板大小，为奇数
+        //x方向方差
+        //Y方向方差
+//        GaussianBlur(src,dst,Size(5,5),0,0);
+//        imwrite("gauss.jpg",dst);
+
+        //中值滤波
+        //src:输入图像
+        //dst::输出图像
+        //模板宽度，为奇数
+//        medianBlur(src,dst,3);
+//        imwrite("med.jpg",dst);
+
+        //均值滤波
+        //src:输入图像
+        //dst:输出图像
+        //模板大小
+        //Point(-1,-1):被平滑点位置，为负值取核中心
+//        blur(src,dst,Size(3,3),Point(-1,-1));
+//        imwrite("mean.jpg",dst);
+
+        //双边滤波
+        //src:输入图像
+        //dst:输入图像
+        //滤波模板半径
+        //颜色空间标准差
+        //坐标空间标准差
+//        bilateralFilter(src,dst,5,10.0,2.0);//这里滤波没什么效果，不明白
